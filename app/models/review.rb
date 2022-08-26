cbce6c66c271823761b5fdfe5b6f99df0726f6f9
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-26 20:16:06 +0300

    many to many

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
 class Review < ActiveRecord::Base
   belongs_to :game
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
 class User < ActiveRecord::Base
-  
+    has_many :reviews
+    has_many :games, through: :reviews 
 end
